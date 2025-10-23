3f374379f03fc74517c2c9f8e66d50643e052a86
Sandula Naveen <sandula.navin@gmail.com>
2025-10-23 18:15:27 +0530

    debug fix for the table creation.

--- bookmyshow/src/main/java/com/navin/bookmyshow/models/ShowSeat.java
+++ bookmyshow/src/main/java/com/navin/bookmyshow/models/ShowSeat.java
@@ -2,10 +2,16 @@ package com.navin.bookmyshow.models;
 
 import java.util.Date;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class ShowSeat extends BaseModel{
     @ManyToOne
     private Show show;
